1e447f337015239937d474bb0803555677a019e7
Ayush Singh <ayushsingh02082004@gmail.com>
2025-12-14 17:10:42 +0530

    uc4 commit completed

--- Employeemanagement/Program.cs
+++ Employeemanagement/Program.cs
@@ -2,13 +2,12 @@
 {
     internal class Program
     {
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
 
             //VARIABLES
             int empHrs = 0;
@@ -18,18 +17,21 @@
             //Computation
             int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else { empHrs = 0; }
-                   
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Employee Wage : " + empWage);
+            
         }
     }
 
